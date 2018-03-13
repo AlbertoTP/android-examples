eadc65f3420eda9b93331ccf13280b2de5356831
AlbertoTP <alberto.tapia.p@gmail.com>
2018-03-13 02:16:58 -0600

    Intent implicito

--- Aplicacion002/app/src/main/java/com/example/alternatif/aplicacion002/SecondActivity.java
+++ Aplicacion002/app/src/main/java/com/example/alternatif/aplicacion002/SecondActivity.java
@@ -1,13 +1,17 @@
 package com.example.alternatif.aplicacion002;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
     private TextView textView;
+    private Button buttonThirdAct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,7 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         textView = findViewById(R.id.textViewMain);
+        buttonThirdAct = findViewById(R.id.buttonThirdAct);
 
         //Tomar los datos del intent
         Bundle bundle = getIntent().getExtras();
@@ -27,5 +32,13 @@ public class SecondActivity extends AppCompatActivity {
             Toast.makeText(SecondActivity.this,"It's Empty",Toast.LENGTH_LONG);
             textView.setText("Empty");
         }
+
+        buttonThirdAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Aplicacion002/app/src/main/java/com/example/alternatif/aplicacion002/ThirdActivity.java
@@ -0,0 +1,106 @@
+package com.example.alternatif.aplicacion002;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    private EditText editTextPhone, editTextWeb;
+    private ImageButton imageButtonPhone, imageButtonWeb, imageButtonCamera;
+    private final int PHONE_CALL_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextWeb = findViewById(R.id.editTextWeb);
+        imageButtonPhone = findViewById(R.id.imageButtonPhone);
+        imageButtonWeb = findViewById(R.id.imageButtonWeb);
+        imageButtonCamera = findViewById(R.id.imageButtonCamera);
+
+        imageButtonPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNumber = editTextPhone.getText().toString();
+                if (phoneNumber != null && !phoneNumber.isEmpty()) {
+                    //comprobar version actual de android
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+                    } else {
+                        olderVersions(phoneNumber);
+                    }
+                }
+                else {
+                    Toast.makeText(ThirdActivity.this,"Empty, please insert a phone number",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            private void olderVersions(String phoneNumber) {
+                Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+                if (checkPermission(Manifest.permission.CALL_PHONE)) {
+                    startActivity(intentCall);
+                } else {
+                    Toast.makeText(ThirdActivity.this, "You decline the access", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    // de donde viene la peticion?
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //caso de telefono
+        switch (requestCode) {
+            case PHONE_CALL_CODE:
+                String permission = permissions[0];
+                int result = grantResults[0];
+
+                if (permission.equals(Manifest.permission.CALL_PHONE)) {
+                    //comprobar si ha sido aceptado o denegado la peticion de servicio
+                    if (result == PackageManager.PERMISSION_GRANTED) {
+                        //concedio permiso
+                        String phoneNumber = editTextPhone.getText().toString();
+                        Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        startActivity(intentCall);
+                    } else{
+                        //no concedio permiso
+                        Toast.makeText(ThirdActivity.this,"You decline the access",Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
+
+    }
+
+    private boolean checkPermission(String permmission){
+        int result = this.checkCallingOrSelfPermission(permmission);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+}
